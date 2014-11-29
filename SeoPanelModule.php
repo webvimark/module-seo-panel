8c1358e9935ebd6e6e2ea9c64992a7098b1e85df
vi mark <webvimark@gmail.com>
2014-11-29 13:22:45 +0100

    translations

--- SeoPanelModule.php
+++ SeoPanelModule.php
@@ -6,30 +6,41 @@ use Yii;
 
 class SeoPanelModule extends \yii\base\Module
 {
+	/**
+	 * Helps to check if translations have been registered already
+	 *
+	 * @var bool
+	 */
+	protected static $_translationsRegistered = false;
+
 	public $controllerNamespace = 'webvimark\modules\SeoPanel\controllers';
 
-
-	public function init()
-	{
-		parent::init();
-
-		$this->registerTranslations();
-	}
-
-	public function registerTranslations()
-	{
-		Yii::$app->i18n->translations['modules/seo-panel/*'] = [
-			'class'          => 'yii\i18n\PhpMessageSource',
-			'sourceLanguage' => 'en',
-			'basePath'       => '@vendor/webvimark/module-seo-panel/messages',
-			'fileMap'        => [
-				'modules/seo-panel/app' => 'app.php',
-			],
-		];
-	}
-
+	/**
+	 * I18N helper
+	 *
+	 * @param string      $category
+	 * @param string      $message
+	 * @param array       $params
+	 * @param null|string $language
+	 *
+	 * @return string
+	 */
 	public static function t($category, $message, $params = [], $language = null)
 	{
+		if ( !static::$_translationsRegistered )
+		{
+			Yii::$app->i18n->translations['modules/seo-panel/*'] = [
+				'class'          => 'yii\i18n\PhpMessageSource',
+				'sourceLanguage' => 'en',
+				'basePath'       => '@vendor/webvimark/module-seo-panel/messages',
+				'fileMap'        => [
+					'modules/seo-panel/app' => 'app.php',
+				],
+			];
+
+			static::$_translationsRegistered = true;
+		}
+
 		return Yii::t('modules/seo-panel/' . $category, $message, $params, $language);
 	}
 }
